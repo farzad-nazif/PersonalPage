e31b019171502c49747c5a9dc16cbe9ba8a2d68e
farzad-nazif <farzadnazif.nz@gmail.com>
2022-05-15 20:30:30 +0100

    6commit

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ document.getElementById('btn').addEventListener('click', (event) => {
   element_headings.style.color = '#ff0000';
 
 });
-document.getElementById('changeimg').addEventListener('click', (event) => {
-  let element_image = document.getElementById('image');
-  element_image.setAttribute("src", 'https://i.ibb.co/9y2jS5X/windows-3f04-FMm-Jqk-unsplash.jpg');
-
-});
