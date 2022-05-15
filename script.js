ad4543a7f1e75ef149c08585c07daf330acdf462
farzad-nazif <farzadnazif.nz@gmail.com>
2022-05-15 13:47:24 +0100

    Firstcommit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', (event) => {
+  let element_headings = document.getElementById('headings');
+  element_headings.style.color = '#ff0000';
+
+});
